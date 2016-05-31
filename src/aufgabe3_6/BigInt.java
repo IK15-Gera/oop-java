718aef9d831ab1661af48d3650e3db7ce0e91c67
Jacob Fricke <jacob@http418.name>
2016-05-31 10:26:49 +0200

    == Aufgabe 3.6
    ++ BigInt.add(BigInt)
    ++ BigInt.set(String)
    ++ BigInt(String)

--- src/aufgabe3_6/BigInt.java
+++ src/aufgabe3_6/BigInt.java
@@ -10,8 +10,32 @@ public class BigInt{
         this.set(v) ;
     }
 
+    BigInt(String v){
+        this.set(v) ;
+    }
+
     public void set(int v){
         this.value = Integer.toString(v) ;
+    }
+
+    public void set(String v){
+        for(int i = 0; i < v.length(); i++){
+            if(v.charAt(i) == '0'
+                    || v.charAt(i) == '1'
+                    || v.charAt(i) == '2'
+                    || v.charAt(i) == '3'
+                    || v.charAt(i) == '4'
+                    || v.charAt(i) == '5'
+                    || v.charAt(i) == '6'
+                    || v.charAt(i) == '7'
+                    || v.charAt(i) == '8'
+                    || v.charAt(i) == '9'){
+                continue ;
+            }else{
+                throw new java.lang.NumberFormatException("illegal char at '"+i+"': "+v.charAt(i)) ;
+            }
+        }
+        this.value = v ;
     }
 
     public String get(){
@@ -20,10 +44,33 @@ public class BigInt{
 
     public BigInt add(BigInt s){
         String valb = s.get() ;
+        String result  = "" ;
 
+
+        int a ;
+        int b ;
+
+        int rest = 0 ;
+        int step ;
         int len = Math.max(valb.length(), this.value.length()) ;
         for(int i = 0; i < len; i++){
+            try{
+                a = Integer.parseInt(""+this.value.charAt(i));
+            }catch(java.lang.IndexOutOfBoundsException e){
+                a = 0 ;
+            }
 
+            try{
+                b = Integer.parseInt(""+valb.charAt(i));
+            }catch(java.lang.IndexOutOfBoundsException e){
+                b = 0 ;
+            }
+
+            step = a + b + rest ;
+            rest = step % 10 ;
+            result = Integer.toString(step/10)+result ;
         }
+
+        return new BigInt(result) ;
     }
 }
